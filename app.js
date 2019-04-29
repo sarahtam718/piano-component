2e96054f8634c38e20c7e4e9d3735d0d387c4bc2
Sarah Tam <sarahtam718@gmail.com>
2019-04-29 14:18:23 -0500

    better on-click

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 function playNote(event) {
+    console.log("what is the id?", event)
     // const audio = document.querySelector(`audio[data-key='${event.keyCode}']`);
     console.log("---------playNote function-------")
     // var audio = document.getElementById("audio");
     // audio.play();
 }
 
-const pianoKey = document.getElementsByClassName("pianoKey");
-console.log("pianoKey const: ", pianoKey)
+$(".pianoKey").click(function () {
+    console.log("i've been clicked");
+    console.log("you clicked me: ", this)
+})
 
-Array.from(pianoKey).forEach(function (element) {
-    element.addEventListener('click', playNote);
-    console.log("what is this?", element)
-});
+// const pianoKey = document.getElementsByClassName("pianoKey");
+// console.log("pianoKey const: ", pianoKey)
+
+// Array.from(pianoKey).forEach(function (element) {
+//     element.addEventListener('click', playNote);
+//     // console.log("what is this?", element)
+// });
 
 // $(".piano").addEventListener('click', playNote)
 
