86c5b4855e9928a6909863bc301bcc85a7d64d55
Sarah Tam <sarahtam718@gmail.com>
2019-04-30 11:31:47 -0500

    can capture array of user's keysPlayed

--- app.js
+++ app.js
@@ -1,38 +1,11 @@
-// function playNote(event) {
-//     const audio = $('this');
-//     console.log("---------playNote function-------", audio)
-//     // var audio = document.getElementById("audio");
-//     // audio.play();
-// }
-
-// var audio = $("audio");
-
-// const audio = $("audio");
-// console.log("audio: ", audio)
-
-// function play(event) {
-//     console.log("audio: ", event)
-//     event.play();
-// }
+const keysPlayed = [];
 
 $(".pianoKey").click(function () {
     var keyData = $(this)[0].id;
-    console.log(keyData);
-    var audioClip = $("#c_octave1_audio")[0];
-    console.log("the sound: ", audioClip);
-    audioClip.play();
-    // playNote(keyData)
-})
-
-// const audio = `${event}`;
-// console.log("---------playNote function-------", audio)
-
-
-// window.addEventListener('keydown', function (e) {
-//     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-//     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-//     if (!audio) return;
-//     audio.currentTime = 0;
-//     audio.play();
-//     key.classList.add('active')
-// });
+    keysPlayed.push(keyData);
+    console.log(keysPlayed)
+    // console.log(keyData);
+    // var audioClip = $("#c_octave1_audio")[0];
+    // console.log("the sound: ", audioClip);
+    // audioClip.play();
+});
